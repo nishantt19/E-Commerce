e94c66df617ea98823fdcc2d33f8a7f230f01174
nishantt19 <nyd44533@gmail.com>
2022-12-07 21:27:33 +0530

    second commit with navigation bar and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
 
-import Directory from "./components/directory/directory.component";
-const  App=()=> {
-
-  
-
-
+const Shop =()=>{
   return (
-    <Directory />
-  );
+    <div>
+      <h1>I am the shop page</h1>
+    </div>
+  )
 }
+
+const App =()=>{
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element ={<Shop />} />
+      </Route>
+    </Routes>
+  )
+}
+
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,38 +3,12 @@ import "./directory.styles.scss";
 import CategoryItem from "../category-item/category-item.component";
 
 
-const categories =[
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
 
-const Directory =()=>{
+
+const Directory =(props)=>{
     return(
         <div className="directory-container" >
-    {categories.map((category)=>{
+    {props.categories.map((category)=>{
       return(
        
         <CategoryItem key={category.id} category= {category}/>
