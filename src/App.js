51829f7de7dfb965d4be1f4e7048527e658d5fbf
nishantt19 <nyd44533@gmail.com>
2022-12-08 17:30:06 +0530

    added the cart option

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-
-const Shop =()=>{
-  return (
-    <div>
-      <h1>I am the shop page</h1>
-    </div>
-  )
-}
+import Shop from "./routes/shop/shop.component";
 
 const App =()=>{
   return (
