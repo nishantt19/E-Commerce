b2c8580e0da2f08275d77c2f526bf43f90878a70
nishantt19 <nyd44533@gmail.com>
2022-12-09 13:51:46 +0530

    completed the checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,9 @@ import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
 import { useContext } from "react";
 
+// this is a hook which gives access to navigation object
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../contexts/cart.context";
 import CartItem from "../cart-item/cart-item.component";
 import { createUserWithEmailAndPassword } from "firebase/auth";
@@ -10,15 +13,21 @@ const CartDropdown = () => {
 
 const {cartItems} = useContext(CartContext);
 
+const navigate = useNavigate();
+
+const goToCheckoutHandler =()=>{
+  navigate('/checkout');
+}
+
   return (
-    <div className="cart-dropdown-container">
+    <div className="cart-dropdown-container" >
       <div className="cart-items" >
         {cartItems.map((item)=>
         <CartItem cartItem={item} key={item.id}/>
         )}
       </div>
 
-      <Button>Go to Checkout</Button>
+      <Button  onClick={goToCheckoutHandler}>Go to Checkout</Button>
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import { useAsyncError } from "react-router-dom";
 
 
 // to check if the item we are going to add is already in the cart or not and if it is in the cart, then just increment the quantity , otherwise add the item to our cart.
@@ -19,12 +20,37 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem =(cartItems, cartItemToRemove)=>{
+    const existingCardItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+      );
+
+      if(existingCardItem.quantity===1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+      }
+
+      return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  }
+
+  const clearCartItem =(cartItems, cartItemToClear)=>{
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
+  }
+
+
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
-  cartCount : 0
+  removeItemFromCart : ()=>{},
+  clearItemFromCart : ()=>{},
+  cartCount : 0,
+  cartTotal :0
 });
 
 export const CartProvider = ({ children }) => {
@@ -33,12 +59,20 @@ export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
   const [cartCount, setCartCount]= useState(0);
+  const [cartTotal, setCartTotal]= useState(0);
+
 
   useEffect(()=>{
 const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity, 0) ;
  setCartCount(newCartCount);
   },
-  [cartItems])
+  [cartItems]);
+
+  useEffect(()=>{
+const newCartTotal = cartItems.reduce((total, cartItem)=> total + cartItem.quantity*cartItem.price, 0) ;
+ setCartTotal(newCartTotal);
+  },
+  [cartItems]);
 
 
 //   function to add item to cart
@@ -46,6 +80,16 @@ const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quant
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen , cartItems, addItemToCart, cartCount};
+//   function to remove a item from the cart
+  const removeItemFromCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
+//   function to clear the item from the cart
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+  };
+
+  const value = { isCartOpen, setIsCartOpen , cartItems, addItemToCart, cartCount, removeItemFromCart, clearItemFromCart, cartTotal};
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
