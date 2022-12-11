7cf5068138ce83cfa337012355b7a52a1df5a7eb
nishantt19 <nyd44533@gmail.com>
2022-12-11 23:35:44 +0530

    used the useReducer in the user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useReducer } from 'react';
 
 import {
   onAuthStateChangedListener,
@@ -10,10 +10,45 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER : 'SET_CURRENT_USER'
+}
 
+const userReducer =(state, action)=>{
+  const {type, payload} = action;
+
+  switch(type){
+    case USER_ACTION_TYPES.SET_CURRENT_USER: 
+    return{
+      ...state,
+      currentUser:  payload
+    }
+     default :
+      throw new Error(`Unhandled type ${type} in userReducer`); 
+    
+  }
+
+}
+
+// we can use both useReducer and useState but useState is more preferable if we only have one or 2 variables to set, and if we have multiple then we should use useReducer as it will be more helpful and the code will be more optimised
+
+
+const INITIAL_STATE = {
+  currentUser : null
+}
+
+export const UserProvider = ({ children }) => {
+  // const [currentUser, setCurrentUser] = useState(null);
+
+
+  const [ state , dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const {currentUser} = state;
+
+  const setCurrentUser =(user)=>{
+    dispatch({type : USER_ACTION_TYPES.SET_CURRENT_USER, payload : user});
+  }
+
+  const value = { currentUser, setCurrentUser };
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
