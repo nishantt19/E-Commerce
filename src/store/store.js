c7d8c799760761b844ac0a2fe9cc77f4a41dba76
Nishant <108404252+nishantt19@users.noreply.github.com>
2022-12-18 12:59:07 +0530

    introduced the reselect library

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,28 @@
+
 import { compose, applyMiddleware, createStore } from "redux";
 
-import logger from "redux-logger";
+// import logger from "redux-logger";
+
+
 
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const loggerMiddleWare = (store)=> (next)=> (action)=>{
+    if(!action.type){
+        return next(action);
+    }
+
+    console.log('type: ' , action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState: ', store.getState());
+
+    next(action);
+
+    console.log('next state: ' , store.getState());
+}
+
+const middleWares = [loggerMiddleWare];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
